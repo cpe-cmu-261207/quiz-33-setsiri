59dcba4d49ad3489421e422e334699e38bb7423e
setsiri <76035222+setsiri@users.noreply.github.com>
2022-09-15 14:07:49 +0700

    added

--- components/PostOwner.jsx
+++ components/PostOwner.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import Comment from "./Comment";
 
-export default function PostOwner(props) {
-  return <div></div>;
+export default function PostOwner() {
+  return (
+    <div>
+      <div className="vstack gap-3">
+        <div className="d-flex align-items-center gap-3">
+          <img
+            src="/profileImages/profileme.jpg"
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <span className="fw-semibold fs-5 text-white">
+            Setsiri Matewin 630610766
+          </span>
+        </div>
+        <span className="text-white">Hello quiz3 #261207 #yoyo</span>
+        <div className="d-flex align-items-center gap-1">
+          <img src="/like.svg" width={20}></img>
+          <span style={{ color: "#B0B3B8" }}>1100 คน</span>
+        </div>
+        <hr className="m-0 border" />
+      </div>
+      <Comment />
+    </div>
+  );
 }
